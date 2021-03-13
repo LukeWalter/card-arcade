b1268c3e454381bfca462b78ef361bcc768ea843
Luke Walter <lwalter5667@gmail.com>
2021-03-13 14:57:35 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash -e
+#!/usr/bin/env sh
 
 rm -rf bin
 mkdir bin
@@ -6,11 +6,7 @@ mkdir bin
 javac -d bin src/cards/standard/Suit.java
 javac -d bin -cp bin src/cards/standard/Card.java
 javac -d bin -cp bin src/cards/standard/Deck.java
-
-javac -d bin -cp bin src/games/blackjack/BLJUtility.java
-javac -d bin -cp bin src/games/blackjack/Player.java
-javac -d bin -cp bin src/games/blackjack/Dealer.java
-javac -d bin -cp bin src/games/blackjack/Blackjack.java
+javac -d bin -cp bin src/cards/standard/Player.java
 
 javac -d bin -cp bin src/play/Driver.java
 java -cp bin play.Driver
